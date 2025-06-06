72a8134c48e3059cdbda70b2bed96a01aeec08f5
MillyDzukou <dzukoumilly2024>
2025-06-06 12:43:29 -0600

    Create a TODO list of all requiremts

--- a4-classroom-MillyDzukou/calgary_dogs.py
+++ a4-classroom-MillyDzukou/calgary_dogs.py
@@ -1,5 +1,5 @@
 # calgary_dogs.py
-# AUTHOR NAME
+# AUTHOR Milly Dzukou
 #
 # A terminal-based application for computing and printing statistics based on given input.
 # Detailed specifications are provided via the Assignment 4 README file.
@@ -10,12 +10,36 @@
 def main():
 
     # Import data here
+    # TODO Create a dataframe with data from CalgaryDogBreeds.xlsx
+    # TODO Do not modify the files, can change index
+    #dog_data =
 
     print("ENSF 692 Dogs of Calgary")
 
     # User input stage
+    # TODO : prompt dog breed(upper case, lower case, mix case)
+    """       check if it's a breed
+            no: KeyError “Dog breed not found in the data. Please try again.” and prompt again
+            yes continue the program
+            end the program after analysis """
 
-    # Data anaylsis stage
+
+    # Data analysis stage
+    #one multi-index Pandas DataFrame, at least one IndexSlice object,
+    # at least one masking operation, at least one grouping operation,
+    # and at least one built-in Pandas or NumPy computational method.
+
+    # TODO: Find and print all years where the selected breed was listed in the top breeds.
+
+    # TODO: Calculate and print the total number of registrations of the selected breed found in the dataset.
+
+    # TODO: Calculate and print the percentage of selected breed registrations out of
+    # the total percentage for each year (2021, 2022, 2023).
+
+    # TODO Calculate and print the percentage of selected breed registrations out of the total three-year percentage.
+
+    # TODO Find and print the months that were most popular for the selected breed registrations. Print all months that tie.
+
 
 if __name__ == '__main__':
     main()
